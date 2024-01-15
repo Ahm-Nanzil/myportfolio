368b1d2b0aadc836b8baf779485421efb3817656
ABU HASIB MUHAMMAD NANZIL <106665861+Ahm-Nanzil@users.noreply.github.com>
2024-01-15 16:27:20 +0600

    mailing add

--- assets/js/load.js
+++ assets/js/load.js
@@ -1,8 +1,9 @@
-window.addEventListener('load', function() {
-    var loadingOverlay = document.getElementById('loading-overlay');
-    var siteContent = document.getElementById('site-content');
-  
-    loadingOverlay.style.display = 'none';
-    siteContent.style.display = 'block';
-  });
-  
+function hideShowContent() {
+  var loadingOverlay = document.getElementById('loading-overlay-site');
+  var siteContent = document.getElementById('mysite-content');
+
+  loadingOverlay.style.display = 'none';
+  siteContent.style.display = 'block';
+}
+
+window.addEventListener('load', hideShowContent);
